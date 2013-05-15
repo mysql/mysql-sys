42edaf0585d17c8b3f76b9111b313eedc809da18
Mark Leith <mark@markleith.co.uk>
2013-05-15 16:13:47 +0100

    Added innodb_buffer_statistics_by_schema and innodb_buffer_statistics_by_table summary views based upon the INFORMATION_SCHEMA.INNODB_BUFFER_PAGE table.

--- ps_helper_56.sql
+++ ps_helper_56.sql
@@ -1267,6 +1267,51 @@ SELECT variable_name, variable_value
  WHERE variable_name LIKE 'perf%lost'
    AND variable_value > 0;
 
+/* View: innodb_buffer_statistics_by_table
+ * 
+ * Summarizes the output of the INFORMATION_SCHEMA.INNODB_BUFFER_PAGE 
+ * table, aggregating by schema and table name
+ *
+ */
+
+DROP VIEW IF EXISTS innodb_buffer_statistics_by_table;
+
+CREATE VIEW innodb_buffer_statistics_by_table AS
+SELECT IF(LOCATE('.', ibp.table_name) = 0, 'InnoDB System', REPLACE(SUBSTRING_INDEX(ibp.table_name, '.', 1), '`', '')) AS object_schema,
+       REPLACE(SUBSTRING_INDEX(ibp.table_name, '.', -1), '`', '') AS object_name,
+       format_bytes(SUM(IF(ibp.compressed_size = 0, 16384, compressed_size))) AS innodb_buffer_allocated,
+       format_bytes(SUM(ibp.data_size)) AS innodb_buffer_data,
+       COUNT(ibp.page_number) AS innodb_buffer_pages,
+       COUNT(IF(ibp.is_hashed = 'YES', 1, 0)) AS innodb_buffer_pages_hashed,
+       COUNT(IF(ibp.is_old = 'YES', 1, 0)) AS innodb_buffer_pages_old,
+       ROUND(SUM(ibp.number_records)/COUNT(DISTINCT ibp.index_name)) AS innodb_buffer_rows_cached 
+  FROM information_schema.innodb_buffer_page ibp 
+ WHERE table_name IS NOT NULL
+ GROUP BY object_schema, object_name
+ ORDER BY SUM(IF(ibp.compressed_size = 0, 16384, compressed_size)) DESC;
+
+/* View: innodb_buffer_statistics_by_schema
+ * 
+ * Summarizes the output of the INFORMATION_SCHEMA.INNODB_BUFFER_PAGE 
+ * table, aggregating by schema
+ *
+ */
+
+DROP VIEW IF EXISTS innodb_buffer_statistics_by_schema;
+
+CREATE VIEW innodb_buffer_statistics_by_schema AS
+SELECT IF(LOCATE('.', ibp.table_name) = 0, 'InnoDB System', REPLACE(SUBSTRING_INDEX(ibp.table_name, '.', 1), '`', '')) AS object_schema,
+       format_bytes(SUM(IF(ibp.compressed_size = 0, 16384, compressed_size))) AS innodb_buffer_allocated,
+       format_bytes(SUM(ibp.data_size)) AS innodb_buffer_data,
+       COUNT(ibp.page_number) AS innodb_buffer_pages,
+       COUNT(IF(ibp.is_hashed = 'YES', 1, 0)) AS innodb_buffer_pages_hashed,
+       COUNT(IF(ibp.is_old = 'YES', 1, 0)) AS innodb_buffer_pages_old,
+       ROUND(SUM(ibp.number_records)/COUNT(DISTINCT ibp.index_name)) AS innodb_buffer_rows_cached 
+  FROM information_schema.innodb_buffer_page ibp 
+ WHERE table_name IS NOT NULL
+ GROUP BY object_schema
+ ORDER BY SUM(IF(ibp.compressed_size = 0, 16384, compressed_size)) DESC;
+
 /*
  * Procedure: only_enable()
  *
