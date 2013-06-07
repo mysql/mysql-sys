e72511c4d945ff211982e5f14651f6bda0ae6c84
Mark Leith <mark@markleith.co.uk>
2013-06-07 15:23:50 +0100

    Fixed order of creation issue:
    ERROR 1146 (42S02) at line 981: Table 'ps_helper.innodb_buffer_statistics_by_table' doesn't exist

--- ps_helper_56.sql
+++ ps_helper_56.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS ps_helper DEFAULT CHARACTER SET utf8;
 
 USE ps_helper;
 
-CREATE OR REPLACE VIEW version AS SELECT '1.0.1';
+CREATE OR REPLACE VIEW version AS SELECT '1.0.2';
 
 /*
  * Function: format_bytes()
@@ -937,6 +937,29 @@ SELECT pst.object_schema AS table_schema,
  GROUP BY pst.object_schema, pst.object_name
  ORDER BY pst.sum_timer_wait DESC;
 
+/* View: innodb_buffer_statistics_by_table
+ * 
+ * Summarizes the output of the INFORMATION_SCHEMA.INNODB_BUFFER_PAGE 
+ * table, aggregating by schema and table name
+ *
+ */
+
+DROP VIEW IF EXISTS innodb_buffer_statistics_by_table;
+
+CREATE VIEW innodb_buffer_statistics_by_table AS
+SELECT IF(LOCATE('.', ibp.table_name) = 0, 'InnoDB System', REPLACE(SUBSTRING_INDEX(ibp.table_name, '.', 1), '`', '')) AS object_schema,
+       REPLACE(SUBSTRING_INDEX(ibp.table_name, '.', -1), '`', '') AS object_name,
+       format_bytes(SUM(IF(ibp.compressed_size = 0, 16384, compressed_size))) AS innodb_buffer_allocated,
+       format_bytes(SUM(ibp.data_size)) AS innodb_buffer_data,
+       COUNT(ibp.page_number) AS innodb_buffer_pages,
+       COUNT(IF(ibp.is_hashed = 'YES', 1, 0)) AS innodb_buffer_pages_hashed,
+       COUNT(IF(ibp.is_old = 'YES', 1, 0)) AS innodb_buffer_pages_old,
+       ROUND(SUM(ibp.number_records)/COUNT(DISTINCT ibp.index_name)) AS innodb_buffer_rows_cached 
+  FROM information_schema.innodb_buffer_page ibp 
+ WHERE table_name IS NOT NULL
+ GROUP BY object_schema, object_name
+ ORDER BY SUM(IF(ibp.compressed_size = 0, 16384, compressed_size)) DESC;
+
 /* 
  * View: schema_table_statistics_with_buffer
  *
@@ -1267,29 +1290,6 @@ SELECT variable_name, variable_value
   FROM information_schema.global_status
  WHERE variable_name LIKE 'perf%lost'
    AND variable_value > 0;
-
-/* View: innodb_buffer_statistics_by_table
- * 
- * Summarizes the output of the INFORMATION_SCHEMA.INNODB_BUFFER_PAGE 
- * table, aggregating by schema and table name
- *
- */
-
-DROP VIEW IF EXISTS innodb_buffer_statistics_by_table;
-
-CREATE VIEW innodb_buffer_statistics_by_table AS
-SELECT IF(LOCATE('.', ibp.table_name) = 0, 'InnoDB System', REPLACE(SUBSTRING_INDEX(ibp.table_name, '.', 1), '`', '')) AS object_schema,
-       REPLACE(SUBSTRING_INDEX(ibp.table_name, '.', -1), '`', '') AS object_name,
-       format_bytes(SUM(IF(ibp.compressed_size = 0, 16384, compressed_size))) AS innodb_buffer_allocated,
-       format_bytes(SUM(ibp.data_size)) AS innodb_buffer_data,
-       COUNT(ibp.page_number) AS innodb_buffer_pages,
-       COUNT(IF(ibp.is_hashed = 'YES', 1, 0)) AS innodb_buffer_pages_hashed,
-       COUNT(IF(ibp.is_old = 'YES', 1, 0)) AS innodb_buffer_pages_old,
-       ROUND(SUM(ibp.number_records)/COUNT(DISTINCT ibp.index_name)) AS innodb_buffer_rows_cached 
-  FROM information_schema.innodb_buffer_page ibp 
- WHERE table_name IS NOT NULL
- GROUP BY object_schema, object_name
- ORDER BY SUM(IF(ibp.compressed_size = 0, 16384, compressed_size)) DESC;
 
 /* View: innodb_buffer_statistics_by_schema
  * 
