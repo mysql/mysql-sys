56c88ac9e6e0795c6caae5ebd005a779846fead3
Mark Leith <mark@markleith.co.uk>
2013-09-17 09:46:40 +0100

    Do not take the mysql schema in to account for unused indexes.

--- views/p_s/schema_unused_indexes.sql
+++ views/p_s/schema_unused_indexes.sql
@@ -28,4 +28,5 @@ SELECT object_schema,
   FROM performance_schema.table_io_waits_summary_by_index_usage 
  WHERE index_name IS NOT NULL
    AND count_star = 0
+   AND object_schema != 'mysql'
  ORDER BY object_schema, object_name;
