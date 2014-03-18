f0738c233069a0e102c25588f37763e5f26bd17e
aadant <arnaud@adant.fr>
2014-03-18 19:15:11 +0000

    Update sys_57.sql

--- sys_57.sql
+++ sys_57.sql
@@ -60,6 +60,7 @@ SOURCE ./views/p_s/io_global_by_wait_by_bytes.sql
 SOURCE ./views/p_s/io_global_by_wait_by_latency.sql
 
 SOURCE ./views/p_s/memory_by_user.sql
+SOURCE ./views/p_s/memory_by_host.sql
 SOURCE ./views/p_s/memory_global_by_current_allocated.sql
 SOURCE ./views/p_s/memory_global_total.sql
 
@@ -82,6 +83,13 @@ SOURCE ./views/p_s/user_summary_by_statement_type.sql
 SOURCE ./views/p_s/user_summary_by_statement_latency.sql
 SOURCE ./views/p_s/user_summary_by_stages.sql
 SOURCE ./views/p_s/user_summary_57.sql
+
+SOURCE ./views/p_s/host_summary_by_file_io_type.sql
+SOURCE ./views/p_s/host_summary_by_file_io.sql
+SOURCE ./views/p_s/host_summary_by_statement_type.sql
+SOURCE ./views/p_s/host_summary_by_statement_latency.sql
+SOURCE ./views/p_s/host_summary_by_stages.sql
+SOURCE ./views/p_s/host_summary_57.sql
 
 SOURCE ./views/p_s/wait_classes_global_by_avg_latency.sql
 SOURCE ./views/p_s/wait_classes_global_by_latency.sql
