a39c5ac31d8dd64258b8401c559b141aad0ec27b
aadant <arnaud@adant.fr>
2014-03-18 22:41:59 +0000

    Update host_summary.sql

--- views/p_s/host_summary.sql
+++ views/p_s/host_summary.sql
@@ -20,7 +20,7 @@
  *
  * mysql> select * from host_summary;
  * +------+------------+-------------------+-----------------------+-------------+----------+-----------------+---------------------+-------------------+--------------+
- * | host | statements | statement_latency | statement_avg_latency | table_scans | file_ios | file_io_latency | current_connections | total_connections | unique_hosts |
+ * | host | statements | statement_latency | statement_avg_latency | table_scans | file_ios | file_io_latency | current_connections | total_connections | unique_users |
  * +------+------------+-------------------+-----------------------+-------------+----------+-----------------+---------------------+-------------------+--------------+
  * | hal1 |       2924 | 00:03:59.53       | 81.92 ms              |          82 |    54702 | 55.61 s         |                   1 |                 1 |            1 |
  * +------+------------+-------------------+-----------------------+-------------+----------+-----------------+---------------------+-------------------+--------------+
@@ -52,7 +52,7 @@ SELECT accounts.host,
        sys.format_time(SUM(io.io_latency)) AS file_io_latency,
        SUM(accounts.current_connections) AS current_connections,
        SUM(accounts.total_connections) AS total_connections,
-       COUNT(DISTINCT user) AS unique_users
+       COUNT(DISTINCT accounts.user) AS unique_users
   FROM performance_schema.accounts
   LEFT JOIN sys.x$host_summary_by_statement_latency AS stmt ON accounts.host = stmt.host
   LEFT JOIN sys.x$host_summary_by_file_io AS io ON accounts.host = io.host
@@ -66,7 +66,7 @@ SELECT accounts.host,
  *
  * mysql> select * from x$host_summary;
  * +------+------------+-------------------+-----------------------+-------------+----------+-----------------+---------------------+-------------------+--------------+
- * | host | statements | statement_latency | statement_avg_latency | table_scans | file_ios | file_io_latency | current_connections | total_connections | unique_hosts |
+ * | host | statements | statement_latency | statement_avg_latency | table_scans | file_ios | file_io_latency | current_connections | total_connections | unique_users|
  * +------+------------+-------------------+-----------------------+-------------+----------+-----------------+---------------------+-------------------+--------------+
  * | hal |       2925 |   239577283481000 |      81906763583.2479 |          83 |    54709 |  55605611965150 |                   1 |                 1 |            1 |
  * +------+------------+-------------------+-----------------------+-------------+----------+-----------------+---------------------+-------------------+--------------+
@@ -98,7 +98,7 @@ SELECT accounts.host,
        SUM(io.io_latency) AS file_io_latency,
        SUM(accounts.current_connections) AS current_connections,
        SUM(accounts.total_connections) AS total_connections,
-       COUNT(DISTINCT accounts.host) AS unique_hosts
+       COUNT(DISTINCT accounts.user) AS unique_users
   FROM performance_schema.accounts
   LEFT JOIN sys.x$host_summary_by_statement_latency AS stmt ON accounts.host = stmt.host
   LEFT JOIN sys.x$host_summary_by_file_io AS io ON accounts.host = io.host
