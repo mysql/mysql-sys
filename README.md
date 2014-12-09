9a062e883f960909152ace7a0a197266d9574007
Jesper Wisborg Krogh <jesper.wisborg.krogh@oracle.com>
2014-12-10 08:44:46 +1100

    More detailed description and an extra example for sys_get_config()

--- README.md
+++ README.md
@@ -1656,6 +1656,18 @@ Returns the value for the requested variable using the following logic:
 1. If the option exists in sys.sys_config return the value from there.
 2. Else fall back on the provided default value.
 
+Notes for using sys_get_config():
+
+* If the default value argument to sys_get_config() is NULL and case 2. is reached, NULL is returned.
+  It is then expected that the caller is able to handle NULL for the given configuration option.
+* The convention is to name the user variables @sys.<name of variable>. It is <name of variable> that
+  is stored in the sys_config table and is what is expected as the argument to sys_get_config().
+* If you want to check whether the configuration option has already been set and if not assign with
+  the return value of sys_get_config() you can use IFNULL(...) (see example below). However this should
+  not be done inside a loop (e.g. for each row in a result set) as for repeated calls where assignment
+  is only needed in the first iteration using IFNULL(...) is expected to be significantly slower than
+  using an IF (...) THEN ... END IF; block (see example below).
+
 ##### Parameters
 
 * in_variable_name (VARCHAR(128)): The name of the config option to return the value for.
@@ -1667,6 +1679,7 @@ VARCHAR(128)
 
 ##### Example
 ```SQL
+-- Get the configuration value from sys.sys_config falling back on 128 if the option is not present in the table.
 mysql> SELECT sys.sys_get_config('statement_truncate_len', 128) AS Value;
 +-------+
 | Value |
@@ -1675,8 +1688,14 @@ mysql> SELECT sys.sys_get_config('statement_truncate_len', 128) AS Value;
 +-------+
 1 row in set (0.00 sec)
 
-mysql> SET @sys.statement_truncate_len = IFNULL(@sys.statement_truncate_len, sys.sys_get_config('statement_truncate_len', 128));
+-- Check whether the option is already set, if not assign - IFNULL(...) one liner example.
+mysql> SET @sys.statement_truncate_len = IFNULL(@sys.statement_truncate_len, sys.sys_get_config('statement_truncate_len', 64));
 Query OK, 0 rows affected (0.00 sec)
+
+-- Check whether the option is already set, if not assign - IF ... THEN ... END IF example.
+IF (@sys.statement_truncate_len IS NULL) THEN
+    SET @sys.statement_truncate_len = sys.sys_get_config('statement_truncate_len', 64);
+END IF;
 ```
 
 
--- functions/sys_get_config.sql
+++ functions/sys_get_config.sql
@@ -31,6 +31,18 @@ CREATE DEFINER='root'@'localhost' FUNCTION sys_get_config (
                 1. If the option exists in sys.sys_config return the value from there.
                 2. Else fall back on the provided default value.
 
+             Notes for using sys_get_config():
+
+                * If the default value argument to sys_get_config() is NULL and case 2. is reached, NULL is returned.
+                  It is then expected that the caller is able to handle NULL for the given configuration option.
+                * The convention is to name the user variables @sys.<name of variable>. It is <name of variable> that
+                  is stored in the sys_config table and is what is expected as the argument to sys_get_config().
+                * If you want to check whether the configuration option has already been set and if not assign with
+                  the return value of sys_get_config() you can use IFNULL(...) (see example below). However this should
+                  not be done inside a loop (e.g. for each row in a result set) as for repeated calls where assignment
+                  is only needed in the first iteration using IFNULL(...) is expected to be significantly slower than
+                  using an IF (...) THEN ... END IF; block (see example below).
+
              Parameters
              -----------
 
@@ -49,6 +61,7 @@ CREATE DEFINER='root'@'localhost' FUNCTION sys_get_config (
              Example
              -----------
 
+             -- Get the configuration value from sys.sys_config falling back on 128 if the option is not present in the table.
              mysql> SELECT sys.sys_get_config(''statement_truncate_len'', 128) AS Value;
              +-------+
              | Value |
@@ -57,8 +70,14 @@ CREATE DEFINER='root'@'localhost' FUNCTION sys_get_config (
              +-------+
              1 row in set (0.00 sec)
 
-             mysql> SET @sys.statement_truncate_len = IFNULL(@statement_truncate_len, sys.sys_get_config(''statement_truncate_len'', 128));
+             -- Check whether the option is already set, if not assign - IFNULL(...) one liner example.
+             mysql> SET @sys.statement_truncate_len = IFNULL(@sys.statement_truncate_len, sys.sys_get_config(''statement_truncate_len'', 64));
              Query OK, 0 rows affected (0.00 sec)
+
+             -- Check whether the option is already set, if not assign - IF ... THEN ... END IF example.
+             IF (@sys.statement_truncate_len IS NULL) THEN
+                 SET @sys.statement_truncate_len = sys.sys_get_config(''statement_truncate_len'', 64);
+             END IF;
             '
     SQL SECURITY INVOKER
     DETERMINISTIC
